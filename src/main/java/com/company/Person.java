34bf41d4a1c6c01bdbfe28410e52e88afba1c674
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-16 16:30:43 +0200

    Update java compiler in pom + add Person class with constructor, getters, setters

---
+++ src/main/java/com/company/Person.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be lower than 0")
+        }
+        this.age = age;
+        return this;
+    }
+}
